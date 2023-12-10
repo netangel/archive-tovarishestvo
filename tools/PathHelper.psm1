d8ef22b6f74345744b1ff3d39a80f9e179a2afe5
Krivopolenov, Artemiy <artemiy.krivopolenov@oracle.com>
2023-12-10 18:14:46 +0100

    Source path processing part 1

---
+++ tools/PathHelper.psm1
@@ -0,0 +1,12 @@
+$PathSeparator = $IsWindows ? "\" : "/" 
+
+Get-FullPathString([string] $FirstPart, [string] $SecondPart)
+{
+    if (-Not ($FirstPart -match "\w$PathSeparator$")) {
+        $FirstPart += $PathSeparator
+    }
+
+    $FirstPart + $SecondPart
+}
+
+Export-ModuleMember -Function Get-FullPathString
